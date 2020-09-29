0e381442fe6cedfaff18970fc1ae78d456d2c96b
Joël Quenneville <joelq@thoughtbot.com>
2020-09-29 00:48:35 -0400

    Generate random ancestors tree
    To make things more interesting, generate the tree of ancestors instead
    of hard-coding it.

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html)
+import Html.Events
+import Random exposing (Generator)
 
 
 main : Program Flags Model Msg
@@ -44,12 +46,43 @@ ancestors =
         (Node "Mother" Empty Empty)
 
 
+ancestorGen : Generator (Tree String)
+ancestorGen =
+    nodeOrEmpty ancestorIdGen
+
+
+ancestorIdGen : Generator String
+ancestorIdGen =
+    Random.int 1 10000
+        |> Random.map (\n -> "Ancestor id-" ++ String.fromInt n)
+
+
+nodeOrEmpty : Generator a -> Generator (Tree a)
+nodeOrEmpty labelGen =
+    Random.weighted ( 50, nodeGen labelGen ) [ ( 50, emptyNodeGen ) ]
+        |> Random.andThen identity
+
+
+nodeGen : Generator a -> Generator (Tree a)
+nodeGen labelGen =
+    Random.map3 Node
+        labelGen
+        (Random.lazy (\_ -> nodeOrEmpty labelGen))
+        (Random.lazy (\_ -> nodeOrEmpty labelGen))
+
+
+emptyNodeGen : Generator (Tree a)
+emptyNodeGen =
+    Random.constant Empty
+
+
 
 -- UPDATE
 
 
 type Msg
-    = Noop
+    = GenerateTreeClicked
+    | ReceiveNewTree (Tree String)
 
 
 init : Flags -> ( Model, Cmd Msg )
@@ -60,17 +93,25 @@ init flags =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Noop ->
-            ( model, Cmd.none )
+        GenerateTreeClicked ->
+            ( model, Random.generate ReceiveNewTree ancestorGen )
+
+        ReceiveNewTree tree ->
+            ( tree, Cmd.none )
 
 
 
 -- VIEW
 
 
-view : Tree String -> Html a
+view : Tree String -> Html Msg
 view tree =
-    foldTree individual unknown tree
+    Html.main_ []
+        [ Html.h1 [] [ Html.text "Ancestor - Origins" ]
+        , Html.button [ Html.Events.onClick GenerateTreeClicked ]
+            [ Html.text "Generate Random Tree" ]
+        , foldTree individual unknown tree
+        ]
 
 
 individual : String -> Html a -> Html a -> Html a
