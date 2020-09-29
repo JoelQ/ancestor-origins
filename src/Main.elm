721c08ea1546c24a6990a32ddd193686c92374ea
Joël Quenneville <joelq@thoughtbot.com>
2020-09-28 23:12:51 -0400

    Render binary tree of ancestors
    This introduces a binary tree type to model ancestors and renders it as
    nested `<ul>`s. Everything is still static.

--- src/Main.elm
+++ src/Main.elm
@@ -5,4 +5,32 @@ import Html exposing (Html)
 
 main : Html a
 main =
-    Html.text "Hello World"
+    view ancestors
+
+
+type Tree a
+    = Node a (Tree a) (Tree a)
+    | Empty
+
+
+ancestors : Tree String
+ancestors =
+    Node "Self"
+        (Node "Father" Empty Empty)
+        (Node "Mother" Empty Empty)
+
+
+view : Tree String -> Html a
+view tree =
+    case tree of
+        Node name father mother ->
+            Html.ul []
+                [ Html.li [] [ Html.text name ]
+                , view father
+                , view mother
+                ]
+
+        Empty ->
+            Html.ul []
+                [ Html.li [] [ Html.text "Unknown" ]
+                ]
